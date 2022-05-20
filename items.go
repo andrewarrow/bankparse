3756bc66a32296c93dc22d474b77b3ccdcaa0f2a
Andrew Arrow <oneone@gmail.com>
2022-05-20 09:07:29 -0700

    limits

--- items.go
+++ items.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func handleItems(tkn *html.Tokenizer) {
+	tdCount := 0
+	dateOn := false
+	afterCount := 0
 	for {
 
 		tt := tkn.Next()
@@ -18,12 +22,36 @@ func handleItems(tkn *html.Tokenizer) {
 		case tt == html.StartTagToken:
 
 			t := tkn.Token()
-			fmt.Println(t, t.Data)
+			if t.Data == "tr" {
+				tdCount = 0
+			} else if t.Data == "td" {
+				tdCount++
+			}
 
 		case tt == html.TextToken:
 
 			t := tkn.Token()
-			fmt.Println("hi", t, t.Data)
+			txt := strings.TrimSpace(t.Data)
+			if txt == "" {
+				continue
+			}
+			if dateOn {
+				if txt == "Print Details" || txt == "Posted" {
+					dateOn = false
+					afterCount = 0
+					continue
+				}
+				fmt.Println(afterCount, txt)
+				afterCount++
+				if afterCount == 2 {
+					dateOn = false
+					afterCount = 0
+				}
+			}
+			if len(txt) == 8 && string(txt[2]) == "/" && string(txt[5]) == "/" { // 05/19/22
+				dateOn = true
+				afterCount = 0
+			}
 
 		}
 
