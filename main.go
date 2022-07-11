2d86872501de860fec3313e90c001d5895aad6ab
Andrew Arrow <oneone@gmail.com>
2022-07-11 07:47:32 -0700

    colors

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func HandleBothDays() {
 	todayPending, todayPosted := handleItems("data/today.txt")
 	fmt.Println("<body>")
 	fmt.Println("<table>")
-	fmt.Printf("<tr><td>%s</td><td></td><td></td></tr>\n", "Pending")
+	fmt.Printf("<tr><td><b>%s</b></td><td></td><td></td></tr>\n", "Pending")
 	total := 0.0
 	for k, v := range todayPending {
 		if yesterdayPending[k] == nil {
@@ -54,11 +54,11 @@ func HandleBothDays() {
 			total += v.AmountInt
 		}
 	}
-	fmt.Printf("<tr><td></td><td>$%.2f</td><td></td></tr>\n", total)
+	fmt.Printf("<tr><td></td><td style='background-color: red;'>$%.2f</td><td></td></tr>\n", total)
 	fmt.Printf("<tr><td></td><td></td><td></td></tr>\n")
 	fmt.Println("</table>")
 	fmt.Println("<table>")
-	fmt.Printf("<tr><td>%s</td><td></td><td></td></tr>\n", "Posted")
+	fmt.Printf("<tr><td><b>%s</b></td><td></td><td></td></tr>\n", "Posted")
 	total = 0.0
 	for k, v := range todayPosted {
 		if yesterdayPosted[k] == nil {
@@ -66,7 +66,7 @@ func HandleBothDays() {
 			total += v.AmountInt
 		}
 	}
-	fmt.Printf("<tr><td></td><td>$%.2f</td><td></td></tr>\n", total)
+	fmt.Printf("<tr><td></td><td style='background-color: red;'>$%.2f</td><td></td></tr>\n", total)
 	fmt.Println("</table>")
 	fmt.Println("</body>")
 }
